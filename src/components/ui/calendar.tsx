3b2786aafc59679f2caf095e03d782d47bf49c06
CVS CHARAN <charan.cvs@gmail.com>
2025-07-07 01:07:10 +0530

    Fix: Type error in Calendar component components prop

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,10 +52,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Icon: {
-          Left: () => <ChevronLeft className="h-4 w-4" />,
-          Right: () => <ChevronRight className="h-4 w-4" />,
-        },
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
